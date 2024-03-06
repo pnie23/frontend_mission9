5f05c669c24fed5706bb7edeaff86033185e11e9
pnie23 <150399640+pnie23@users.noreply.github.com>
2024-03-06 12:37:01 -0700

    Everything for working REACT app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import json file and assign to variable jsonData
+import jsonData from './CollegeBasketballTeams.json';
+
+//set the interface for Teams, import all headers even if not used right now,
+// good practice to have all set if wanted to be used later
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+//set the header for the page
+function HeaderIntro() {
+  return (
+    <header>
+      <h1>March Madness Prep</h1>
+      <h2>-</h2>
+      <h2>NCAA Men's Basketball Teams</h2>
+    </header>
+  );
+}
+
+//component for importing data and formatting the output
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <br></br>
+        <h2>{oneTeam.school}</h2>
+        <h3>{oneTeam.name}</h3>
+        <h3>
+          {oneTeam.city}, {oneTeam.state}
+        </h3>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+//function for gathering the data for each team for outputting
+function TeamList() {
+  return (
+    <div>
+      {/*Pull json data using the variable jsonData and the table name to map*/}
+      {jsonData.teams.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
+  //declare variable for switchin the vie of all teams on/off
+  const [showTeamList, setShowTeamList] = useState(false);
+
+  //function for checking when the button is to show or hide TeamList()
+  const toggleTeamList = () => {
+    setShowTeamList(!showTeamList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderIntro />
+      <br></br>
+      <div>
+        {/* Button to toggle showing TeamList */}
+        <button onClick={toggleTeamList}>
+          {showTeamList ? 'Hide All Teams' : 'Show All Teams'}
+        </button>
+
+        {/* Conditionally render TeamList based on showTeamList state */}
+        {showTeamList && <TeamList />}
+      </div>
     </div>
   );
 }
